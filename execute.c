51ac35221659bc6acfda3cd28b6142ba294fc211
KhairiTab <taboubikhairi68@gmail.com>
2023-08-25 05:35:52 +0100

    test

--- execute.c
+++ execute.c
@@ -6,23 +6,22 @@
  */
 void execute_command(char **args)
 {
-    pid_t child_pid = fork();
-
-    if (child_pid == -1)
-    {
-        perror("Fork error");
-        exit(EXIT_FAILURE);
-    }
-    else 
-    if (child_pid == 0)
-    {
-        execvp(args[0], args);
-        perror("Command execution error");
-        exit(EXIT_FAILURE);
-    }
-    else
-    {
-        int child_status;
-        waitpid(child_pid, &child_status, 0);
-    }
+pid_t child_pid = fork();
+if (child_pid == -1)
+{
+perror("Fork error");
+exit(EXIT_FAILURE);
+}
+else
+if (child_pid == 0)
+{
+execvp(args[0], args);
+perror("Command execution error");
+exit(EXIT_FAILURE);
+}
+else
+{
+int child_status;
+waitpid(child_pid, &child_status, 0);
+}
 }

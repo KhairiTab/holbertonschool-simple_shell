bab8c7fad794aaf8f00a4ff54b9a0dd1b06bc2c5
Khaled Jallouli <135613251+Khaled-J7@users.noreply.github.com>
2023-08-25 23:02:23 +0000

    main && tokenize files betty style fixing

--- main.c
+++ main.c
@@ -1,61 +1,52 @@
 #include "shell.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-
+/**
+ *main function - description
+ */
 int main(void)
 {
-    char *line;
-    char **args;
-    pid_t child_pid;
-    int i;
+	char *line;
+	char **args;
+	pid_t child_pid;
+	int i;
 
-    while (1)
-    {
-        line = prompt();
-        if (line == NULL)
-        {
-            printf("\nExiting shell...\n");
-            break;
-        }
+	while (1)
+	{
+		line = prompt();
+		if (line == NULL)
+		{
+			printf("\nExiting shell...\n");
+			break;
+		}
+		args = parse_command(line);
+		if (args == NULL)
+		{
+			free(line);
+			break;
+		}
+		child_pid = fork();
 
-        args = parse_command(line);
-        if (args == NULL)
-        {
-            free(line);
-            break;
-        }
-
-        child_pid = fork();
-
-        if (child_pid == -1)
-        {
-            perror("Fork error");
-            exit(EXIT_FAILURE);
-        }
-        else if (child_pid == 0)
-        {
-            execvp(args[0], args);
-
-            perror("Command execution error");
-            exit(EXIT_FAILURE);
-        }
-        else
-        {
+		if (child_pid == -1)
+		{
+			perror("Fork error");
+			exit(EXIT_FAILURE);
+		}
+		else if (child_pid == 0)
+		{
+			execvp(args[0], args);
+			perror("Command execution error");
+			exit(EXIT_FAILURE);
+		}
+		else
+		{
             int child_status;
-            waitpid(child_pid, &child_status, 0);
-
-            for (i = 0; args[i] != NULL; i++)
-            {
-                free(args[i]);
-            }
-            free(args);
-        }
-
-        free(line);
-    }
-
-    return 0;
+			waitpid(child_pid, &child_status, 0);
+			for (i = 0; args[i] != NULL; i++)
+			{
+				free(args[i]);
+			}
+			free(args);
+		}
+		free(line);
+	}
+	return (0);
 }
--- tokenize.c
+++ tokenize.c
@@ -1,54 +1,50 @@
 #include "shell.h"
-
+/**
+ *parse_command - tokenize function in shell project
+ *@line: input string
+ */
 char **parse_command(char *line)
 {
-    char **args = NULL;
-    char *arg_copy = NULL;
-    int arg_count = 0;
-    char *arg;
-    int i;
+	char **args = NULL;
+	char *arg_copy = NULL;
+	int arg_count = 0;
+	char *arg;
+	int i;
 
-    arg_copy = strdup(line);
-    if (!arg_copy)
-    {
-        perror("Memory allocation error");
-        exit(EXIT_FAILURE);
-    }
-
-    arg = strtok(arg_copy, TOKEN_DELIMITERS);
-    while (arg != NULL)
-    {
-        arg_count++;
-        arg = strtok(NULL, TOKEN_DELIMITERS);
-    }
-
-    args = malloc(sizeof(char *) * (arg_count + 1));
-    if (!args)
-    {
-        perror("Memory allocation error");
-        exit(EXIT_FAILURE);
-    }
-
-    arg = strtok(line, TOKEN_DELIMITERS);
-    for (i = 0; i < arg_count; i++)
-    {
-        args[i] = strdup(arg);
-        if (!args[i])
-        {
-            perror("Memory allocation error");
-            exit(EXIT_FAILURE);
-        }
-        arg = strtok(NULL, TOKEN_DELIMITERS);
-    }
-    args[arg_count] = NULL;
-
-    free(arg_copy);
-
-    if (arg_count > 0 && strcmp(args[0], "exit") == 0)
-    {
-        free(args);
-        return NULL;
-    }
-
-    return args;
+	arg_copy = strdup(line);
+	if (!arg_copy)
+	{
+		exit(EXIT_FAILURE);
+	}
+	arg = strtok(arg_copy, TOKEN_DELIMITERS);
+	while (arg != NULL)
+	{
+		arg_count++;
+		arg = strtok(NULL, TOKEN_DELIMITERS);
+	}
+	args = malloc(sizeof(char *) * (arg_count + 1));
+	if (!args)
+	{
+		perror("Memory allocation error");
+		exit(EXIT_FAILURE);
+	}
+	arg = strtok(line, TOKEN_DELIMITERS);
+	for (i = 0; i < arg_count; i++)
+	{
+		args[i] = strdup(arg);
+		if (!args[i])
+		{
+			perror("Memory allocation error");
+			exit(EXIT_FAILURE);
+		}
+		arg = strtok(NULL, TOKEN_DELIMITERS);
+	}
+	args[arg_count] = NULL;
+	free(arg_copy);
+	if (arg_count > 0 && strcmp(args[0], "exit") == 0)
+	{
+		free(args);
+		return (NULL);
+	}
+	return (args);
 }

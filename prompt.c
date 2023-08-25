493d1877c5b1ffe80cec4fdee2a08e5242edee6d
Khaled Jallouli <135613251+Khaled-J7@users.noreply.github.com>
2023-08-25 22:50:14 +0000

    prompt.c betty fixing

--- prompt.c
+++ prompt.c
@@ -1,31 +1,32 @@
 #include "shell.h"
 
 /**
- * prompt - Display a prompt and get user input
+ *prompt - Display a prompt and get user input
  *
- * Return: The user's command line input
+ *Return: The user's command line input
  */
 char *prompt(void)
 {
-    char *line = NULL;
-    size_t bufsize = 0;
-    ssize_t chars_read;
+	char *line = NULL;
+	size_t bufsize = 0;
+	ssize_t chars_read;
 
-    write(STDOUT_FILENO, "$ ", 2);
-    chars_read = getline(&line, &bufsize, stdin);
-    if (chars_read == -1)
-    {
-        if (feof(stdin))
-        {
-            free(line);
-            line = NULL;
-            write(STDOUT_FILENO, "\n", 1);
-        }
-        else
-        {
-            perror("Error reading input");
-            exit(EXIT_FAILURE);
-        }
-    }
-    return line;
+	write(STDOUT_FILENO, "$ ", 2);
+	chars_read = getline(&line, &bufsize, stdin);
+	if (chars_read == -1)
+	{
+		if (feof(stdin))
+		{
+			free(line);
+			line = NULL;
+			write(STDOUT_FILENO, "\n", 1);
+		}
+		else
+		{
+			perror("Error reading input");
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	return (line);
 }

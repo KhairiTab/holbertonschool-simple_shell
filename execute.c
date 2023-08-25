28426a319e7611ee45949dc530b5b0f3e7ae9599
KhairiTab <taboubikhairi68@gmail.com>
2023-08-25 05:24:35 +0100

    test

--- execute.c
+++ execute.c
@@ -5,24 +5,23 @@
  * @args: The array of command and arguments
  */
 void execute_command(char **args)
-{ 
+{
     pid_t child_pid = fork();
-    
+
     if (child_pid == -1)
-        {
+    {
         perror("Fork error");
         exit(EXIT_FAILURE);
-        }
-        else if (child_pid == 0)
-        {
+    }
+    else if (child_pid == 0)
+    {
         execvp(args[0], args);
         perror("Command execution error");
         exit(EXIT_FAILURE);
-        }
-        else
-        {
+    }
+    else
+    {
         int child_status;
         waitpid(child_pid, &child_status, 0);
-        }
-    
+    }
 }
